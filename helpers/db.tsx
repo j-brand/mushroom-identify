175964539d247098627bec20b6ee463463c8dc7e
j-brand <johannes@blackbeetle.de>
2025-05-16 08:25:28 +0200

    last changes

--- helpers/db.tsx
+++ helpers/db.tsx
@@ -3,10 +3,10 @@ import { Document, MongoClient } from "mongodb";
 export async function connectToDatabase(): Promise<MongoClient> {
   const user = process.env.MONGODB_USER;
   const password = process.env.MONGODB_PASSWORD;
-  const cluster = process.env.MONGODB_CLUSTER;
+  //const cluster = process.env.MONGODB_CLUSTER;
   const database = process.env.MONGODB_DATABASE;
 
-  const client = await MongoClient.connect(`mongodb+srv://${user}:${password}@${cluster}/${database}?retryWrites=true&w=majority`);
+  const client = await MongoClient.connect(`mongodb+srv://${user}:${password}@${database}?retryWrites=true&w=majority`);
 
   return client;
 }
